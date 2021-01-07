cb513471663dd4b88ccdf25b45b7316fd0a9a42c
viacheslavOrel <58309408+viacheslavOrel@users.noreply.github.com>
2021-01-07 16:19:34 +0200

    change task4

--- task4.js
+++ task4.js
@@ -1,14 +1,20 @@
 module.exports = (arr1, arr2) => {
-    arr1.sort((a, b) => {
-        const indexA = arr2.findIndex(item => item === a);
-        const indexB = arr2.findIndex(item => item === b);
-
-        if (indexA === -1  && indexB === -1) return a - b;
-        if (indexA === -1) return 1;
-        if (indexB === -1) return -1;
-
-        return indexA - indexB;
-    });
+    arr1.sort((a, b) => sortByArray(arr2, a, b));
 
     return arr1;
+}
+
+function sortByArray(arr, value1, value2) {
+    const indexA = getIndex(arr, value1);
+    const indexB = getIndex(arr, value2);
+
+    if (indexA === -1 && indexB === -1) return value1 - value2;
+    if (indexA === -1) return 1;
+    if (indexB === -1) return -1;
+
+    return indexA - indexB;
+}
+
+function getIndex(arr, value) {
+    return arr.findIndex(item => item === value);
 }
